81d26b511914deb902a581f817c7969454517438
ShirelVega <shirelvega0@gmail.com>
2025-11-18 17:18:04 -0800

    Create BankAccount.cpp
    This is a draft

---
+++ BankAccount.cpp
@@ -0,0 +1,18 @@
+//The BankAccount class should have a private string member for account_holder and a private double member for balance.
+
+//The constructor should initialize these members.
+
+//The display_balance function must be marked with const.
+
+//The apply_interest function must be declared as a friend of the BankAccount class within the class definition.
+
+//The apply_interest function should accept a BankAccount object reference and an interest rate (double) as parameters.
+
+//In your main function:
+//Create a BankAccount object.
+
+//Call display_balance to show the initial balance.
+
+//Call the apply_interest friend function to update the balance.
+
+//Call display_balance again to show the updated balance.
